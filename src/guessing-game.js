b24fd556f2f0ed72861e2dba3b4429c9d39cdd30
Ckiv <baltazar1995>
2021-09-06 18:54:45 +0300

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+        this.max = 0;
+        this.min = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        for (var i = min; i <= max; i++) {
+            this.arr.push(i);
+        }
+        this.max = this.arr.length - 1;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.min + this.max) / 2);
+        return this.arr[this.middle];
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
